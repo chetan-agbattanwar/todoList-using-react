701d10aec2e4aaa9e0d0188fbc8634619f906f8a
chetan <chetanagbattanwar26@gmail.com>
2024-08-03 11:42:55 +0530

    updated currentDate component

--- todoList/src/components/CurrentDate.jsx
+++ todoList/src/components/CurrentDate.jsx
@@ -4,6 +4,11 @@ function CurrentDate() {
 
     const [currentDate, setCurrentDate] = useState(new Date());
 
+    const months = [
+        "January", "February", "March", "April", "May", "June", 
+        "July", "August", "September", "October", "November", "December"
+    ]
+
     useEffect(() => {
        setInterval(() => {
         setCurrentDate(new Date())
@@ -13,7 +18,7 @@ function CurrentDate() {
     
   return (
     <center>
-    <span style={{marginRight:"10px" }}>Date : {currentDate.toLocaleDateString()}</span>
+    <span style={{marginRight:"10px" }}>Date : {currentDate.getDate()}-{months[currentDate.getMonth()]}-{currentDate.getFullYear()}</span>
     <span>Time : {currentDate.toLocaleTimeString()}</span>
     </center>
   )
